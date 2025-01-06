8b458f54df3b1c13d1db30a26812e1756525631b
Jonathan de Jong <jonathandejong02@gmail.com>
2025-01-06 14:49:24 +0100

    add jo-ker

--- main.lua
+++ main.lua
@@ -267,15 +267,82 @@ local jokers = {
     },
 
     jo_ker = {
-        name = "Jo(ker)",
         sprite = SP .. "jo-ker",
         rarity = 2,
         blueprint_compat = true,
         cost = 4,
 
-        -- 1 in a 20 chance to level up all poker hands after a round
+        config = { extra = { odds = 20 } },
 
-        -- TODO
+        loc_txt = {
+            name = "Jo(ker)",
+            text = {
+                "{C:green}#1# in #2#{} chance to",
+                "{C:attention}level up all poker hands{}",
+                "after a round",
+            }
+        },
+
+        loc_vars = function(self, info_queue, card)
+            return { vars = { (G.GAME.probabilities.normal or 1), card.ability.extra.odds } }
+        end,
+
+        -- 1 in a 20 chance to level up all poker hands after a round
+        calculate = function(self, card, context)
+            if context.end_of_round and not context.individual and not context.repetition then
+                if pseudorandom('jo-ker') < G.GAME.probabilities.normal / card.ability.extra.odds then
+                    local this_card = context.blueprint_card or card
+
+                    local function upgrade_all_hands(with_card)
+                        card_eval_status_text(with_card, 'extra', nil, nil, nil,
+                            { message = localize('k_upgrade_ex') })
+
+                        update_hand_text({ sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3 },
+                            { handname = localize('k_all_hands'), chips = '...', mult = '...', level = '' })
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'after',
+                            delay = 0.2,
+                            func = function()
+                                play_sound('tarot1')
+                                with_card:juice_up(0.8, 0.5)
+                                G.TAROT_INTERRUPT_PULSE = true
+                                return true
+                            end
+                        }))
+                        update_hand_text({ delay = 0 }, { mult = '+', StatusText = true })
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'after',
+                            delay = 0.9,
+                            func = function()
+                                play_sound('tarot1')
+                                with_card:juice_up(0.8, 0.5)
+                                return true
+                            end
+                        }))
+                        update_hand_text({ delay = 0 }, { chips = '+', StatusText = true })
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'after',
+                            delay = 0.9,
+                            func = function()
+                                play_sound('tarot1')
+                                with_card:juice_up(0.8, 0.5)
+                                G.TAROT_INTERRUPT_PULSE = nil
+                                return true
+                            end
+                        }))
+                        update_hand_text({ sound = 'button', volume = 0.7, pitch = 0.9, delay = 0 }, { level = '+1' })
+                        delay(1.3)
+                        for k, v in pairs(G.GAME.hands) do
+                            level_up_hand(with_card, k, true)
+                        end
+                        update_hand_text({ sound = 'button', volume = 0.7, pitch = 1.1, delay = 0 },
+                            { mult = 0, chips = 0, handname = '', level = '' })
+                    end
+
+                    upgrade_all_hands(this_card)
+                end
+            end
+        end
 
     },
 
